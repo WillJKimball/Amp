c4d3854d7ce440a13add0426dda4bb1741e02a9f
Will Kimball <willkimball8@gmail.com>
2024-06-19 22:13:57 -0400

    more updates

--- src/component/Works/works.js
+++ src/component/Works/works.js
@@ -13,12 +13,24 @@ const Works = () => {
             <h2 className="worksTitle">My Portfolio</h2>
             <span className="worksDesc">The following images provide directions to my projects housed on my github account.</span>
             <div className="worksImgs">
-                <img src={Portfolio1} alt="" className="worksImg"/>
-                <img src={Portfolio2} alt="" className="worksImg"/>
-                <img src={Portfolio3} alt="" className="worksImg"/>
-                <img src={Portfolio4} alt="" className="worksImg"/>
-                <img src={Portfolio5} alt="" className="worksImg"/>
-                <img src={Portfolio6} alt="" className="worksImg"/>
+                <a href="Spatial Transformers Research Project.pdf" target="_blank">
+                    <img src={Portfolio1} alt="Project 1" className="worksImg"/>
+                </a>
+                <a href="https://github.com/WillJKimball/CollegetoNBA.git" target="_blank" rel="noopener noreferrer">
+                    <img src={Portfolio2} alt="Project 2" className="worksImg"/>
+                </a>
+                <a href="https://github.com/WillJKimball/ScheduleApp.git" target="_blank" rel="noopener noreferrer">
+                    <img src={Portfolio3} alt="Project 3" className="worksImg"/>
+                </a>
+                <a href="https://github.com/WillJKimball/project4" target="_blank" rel="noopener noreferrer">
+                    <img src={Portfolio4} alt="Project 4" className="worksImg"/>
+                </a>
+                <a href="https://github.com/WillJKimball/project5" target="_blank" rel="noopener noreferrer">
+                    <img src={Portfolio5} alt="Project 5" className="worksImg"/>
+                </a>
+                <a href="https://github.com/WillJKimball/project6" target="_blank" rel="noopener noreferrer">
+                    <img src={Portfolio6} alt="Project 6" className="worksImg"/>
+                </a>
             </div>
             <button className="worksBtn">See More</button>
         </section>
