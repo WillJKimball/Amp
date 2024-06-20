9650b06d68b266a05fac7eec470be33cb7901978
Will Kimball <willkimball8@gmail.com>
2024-06-19 22:34:54 -0400

    image and link updates

--- src/component/Works/works.js
+++ src/component/Works/works.js
@@ -1,11 +1,8 @@
 import React from "react";
 import './works.css';
-import Portfolio1 from '../../assets/portfolio-1.png';
-import Portfolio2 from '../../assets/portfolio-2.png';
-import Portfolio3 from '../../assets/portfolio-3.png';
-import Portfolio4 from '../../assets/portfolio-4.png';
-import Portfolio5 from '../../assets/portfolio-5.png';
-import Portfolio6 from '../../assets/portfolio-6.png';
+import Transformer from '../../assets/bumble.png';
+import Anthony from '../../assets/anthony.png';
+import Clock from '../../assets/clock.png';
 
 const Works = () => {
     return(
@@ -13,23 +10,14 @@ const Works = () => {
             <h2 className="worksTitle">My Portfolio</h2>
             <span className="worksDesc">The following images provide directions to my projects housed on my github account.</span>
             <div className="worksImgs">
-                <a href="Spatial Transformers Research Project.pdf" target="_blank">
-                    <img src={Portfolio1} alt="Project 1" className="worksImg"/>
+                <a href="https://github.com/WillJKimball/WillJKimball.github.io/blob/main/Spatial%20Transformers%20Research%20Project.pdf" target="_blank" rel="noopener noreferrer">
+                    <img src={Transformer} alt="Project 1" className="worksImg"/>
                 </a>
                 <a href="https://github.com/WillJKimball/CollegetoNBA.git" target="_blank" rel="noopener noreferrer">
-                    <img src={Portfolio2} alt="Project 2" className="worksImg"/>
+                    <img src={Anthony} alt="Project 2" className="worksImg"/>
                 </a>
                 <a href="https://github.com/WillJKimball/ScheduleApp.git" target="_blank" rel="noopener noreferrer">
-                    <img src={Portfolio3} alt="Project 3" className="worksImg"/>
-                </a>
-                <a href="https://github.com/WillJKimball/project4" target="_blank" rel="noopener noreferrer">
-                    <img src={Portfolio4} alt="Project 4" className="worksImg"/>
-                </a>
-                <a href="https://github.com/WillJKimball/project5" target="_blank" rel="noopener noreferrer">
-                    <img src={Portfolio5} alt="Project 5" className="worksImg"/>
-                </a>
-                <a href="https://github.com/WillJKimball/project6" target="_blank" rel="noopener noreferrer">
-                    <img src={Portfolio6} alt="Project 6" className="worksImg"/>
+                    <img src={Clock} alt="Project 3" className="worksImg"/>
                 </a>
             </div>
             <button className="worksBtn">See More</button>
