4164bdbce132fad5f4be7dc8ceb06c54bda35563
Will Kimball <willkimball8@gmail.com>
2024-09-09 20:00:50 -0400

    Resume

--- src/component/Intro/intro.js
+++ src/component/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
             <div className="introContent">
                 <span className="hello">Hello,</span>
                 <span className="introText">I'm<span className="introName"> Will Kimball, </span> <br />A Software Developer</span>
-                <p className="introPara">I am a rising third year computer scientist and statististician at University of Virginia</p>
+                <p className="introPara">I am a third year computer scientist and statististician at University of Virginia</p>
                 <Link><button className="btn"><img src={btnImg} alt ="Hire Me" className="btnImg"></img> Hire Me </button></Link>
             </div>
             <img src={bg} alt="Profile" className="bg" />
