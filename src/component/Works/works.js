acd61349af9555ebdce2fa836a14d1fdd95fe1d3
Will Kimball <willkimball8@gmail.com>
2025-10-13 13:56:53 -0400

    web

--- src/component/Works/works.js
+++ src/component/Works/works.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './works.css';
-import Transformer from '../../assets/bumble.png';
-import Spread from '../../assets/SpreadPrediction.png';
+import NFL from '../../assets/nfl.png';
+import Spread from '../../assets/spread.png';
 import RunItBack from '../../assets/RunItBackLogo.jpg';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 
@@ -10,24 +10,24 @@ const Works = () => {
         {
             img: Spread,
             link: "https://github.com/WillJKimball/STATMLFinal",
-            title: "NCAAB Spread Prediction Project"
+            title: "NCAAB ML Spread Prediction Project"
+        },
+        {
+            img: NFL,
+            link: "https://github.com/WillJKimball/NFLReceivingYardsML",
+            title: "NFL Receiving Yards ML Prediction Project"
         },
         {
             img: RunItBack,
             link: "https://drive.google.com/drive/folders/1UWo_gAsyUs1r_EWZHldOqHpd_HLowb8m?usp=drive_link",
-            title: "Run It Back Project"
-        },
-        {
-            img: Transformer,
-            link: "https://github.com/WillJKimball/WillJKimball.github.io/blob/main/Spatial%20Transformers%20Research%20Project.pdf",
-            title: "Spatial Transformers Research"
+            title: "Run It Back Project (SaaS)"
         },
     ];
 
     return(
         <section id='works'>
             <h2 className="worksTitle">My Portfolio</h2>
-            <span className="worksDesc">The following projects showcase my technical expertise and research experience.</span>
+            <span className="worksDesc">The following projects showcase my technical expertise.</span>
             <div className="worksImgs">
                 {projects.map((project, index) => (
                     <div className="workItem" key={index}>
